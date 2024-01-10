b195788fc8204fbac1dceea71789932e14d6e6d9
Clay Whitelytning <cwhitelytning@gmail.com>
2024-01-10 05:02:12 +0300

    Fixed Doxygen in instruction.h

--- src/include/zodiac/instruction/instruction.h
+++ src/include/zodiac/instruction/instruction.h
@@ -1,9 +1,9 @@
 /**
- * @file zodiac_instruction.h
+ * @file instruction.h
  * @brief Definition of instruction-related types and structures for Zodiac.
  *
  * This header file defines the types and structures used to create and manage
- * instructions within the Zodiac system. An instruction is composed of an operation
+ * instructions within the Zodiac system. An instruction is composed of an instruction
  * header, which includes controller and operation indices, and a set of operands.
  * The exact definition and usage of controller and operation indices are determined
  * by the Zodiac platform's requirements.
@@ -23,7 +23,7 @@ typedef uint8_t controller_index_t;
 /**
  * @brief Operation index type definition.
  * Represents the operation to be performed by the controller specified by the
- * controller_index within an operation header.
+ * controller_index within an instruction header.
  */
 typedef uint8_t operation_index_t;
 
@@ -34,22 +34,22 @@ typedef uint8_t operation_index_t;
 typedef uint8_t operands_length_t;
 
 /**
- * @brief Structure that defines the operation header of an instruction.
- * This header contains the indices required to identify the operation and
+ * @brief Structure that defines the instruction header of an instruction.
+ * This header contains the indices required to identify the instruction and
  * the length of operands that this operation takes.
  */
-typedef struct operation_header_s {
-    controller_index_t controller_index; ///< Index of the controller
-    operation_index_t operation_index;   ///< Index of the operation to be performed
-    operands_length_t operands_length;   ///< Number of operands following the header
-} operation_header_t;
+typedef struct instruction_header_s {
+    controller_index_t controller_index;    ///< Index of the controller
+    operation_index_t operation_index;      ///< Index of the operation to be performed
+    operands_length_t number_of_operands;   ///< Number of operands following the header
+} instruction_header_t;
 
 /**
- * @brief Operand type definition.
+ * @brief Instruction operand type definition.
  * Represents a single operand in an instruction. The exact meaning and size of the
  * operand are defined by the controller that interprets it.
  */
-typedef uint8_t operand_t;
+typedef uint8_t instruction_operand_t;
 
 /**
  * @brief Max number of instruction operands.
@@ -59,20 +59,20 @@ typedef uint8_t operand_t;
 #define MAX_NUMBER_OF_INSTRUCTION_OPERANDS 256
 
 /**
- * @brief Operands type definition.
+ * @brief Instruction operands type definition.
  * Defines an array type that can hold the maximum number of operands defined
  * by MAX_NUMBER_OF_INSTRUCTION_OPERANDS.
  */
-typedef operand_t operands_t[MAX_NUMBER_OF_INSTRUCTION_OPERANDS];
+typedef instruction_operand_t instruction_operands_t[MAX_NUMBER_OF_INSTRUCTION_OPERANDS];
 
 /**
  * @brief Structure that defines an instruction.
- * An instruction consists of an operation header and a set of operands. The operands
+ * An instruction consists of an instruction header and a set of operands. The operands
  * are stored in an array sized to the maximum number of operands allowed.
  */
 typedef struct instruction_s {
-    operation_header_t operation_header; ///< Operation header component of the instruction
-    operands_t operands;                 ///< Operands of the instruction
+    instruction_header_t header;        ///< Instruction header component of the instruction
+    instruction_operands_t operands;    ///< Operands of the instruction
 } instruction_t;
 
 #endif // ZODIAC_INSTRUCTION_H
